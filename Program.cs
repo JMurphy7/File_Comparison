670a84b398f672cd34e85af3c85540df4943b464
Joseph Murphy <joeymurphy@live.co.uk>
2020-03-19 13:07:47 +0000

    Added file comparison that compares the size and the contents. Added bool to check file similarity. Comments more comprehensive.

--- Program.cs
+++ Program.cs
@@ -7,31 +7,30 @@ namespace File_Comparison
     {
         public static string f1 = "";
         public static string f2 = "";
+        public static bool same = true;
         static void Main(string[] args)
         {
-            string input_ = Console.ReadLine();
-            UI(input_);
+            UI(Console.ReadLine());
             //string inpt = Console.ReadLine();
             //string inpt2 = Console.ReadLine();
             //readFile(inpt, inpt2);
         }
-        static void readFile(string inpt, string inpt2)
+        static void readFile(string inpt, string inpt2) // Reads the contents of the file in to be inspected by the dataCheck() function.
         {
             try
             {
-                using(StreamReader a = new StreamReader(inpt))
+                using(StreamReader a = new StreamReader(inpt)) // This could be its own function, and it could just be iterated. 
                 {
                     string line;
-                    while((line = a.ReadLine()) != null)
+                    while((line = a.ReadLine()) != null) // Reads line by line
                     {
-                        Console.WriteLine(line);
-                        f1 = f1 + line;
+                        f1 = f1 + line; // Adds to a string.
                     }
                 }
             }
             catch(Exception e)
             {
-                Console.WriteLine("The file could not be read.");
+                Console.WriteLine("The file could not be read."); // If the file isnt there, tell the user.
             }
             try
             {
@@ -40,7 +39,6 @@ namespace File_Comparison
                     string line;
                     while ((line = a.ReadLine()) != null)
                     {
-                        Console.WriteLine(line);
                         f2 = f2 + line;
                     }
                 }
@@ -49,13 +47,12 @@ namespace File_Comparison
             {
                 Console.WriteLine("The file could not be read.");
             }
-            Console.WriteLine(f1);
-            Console.WriteLine(f2);
+
         }
         static void UI(string conInput)
         {
             string[] command = conInput.Split(" ");
-            switch(command[0]){
+            switch(command[0]){                                         // Switch statement is used to make it harder to break with bogus input, whilst also making it easier to read when adding new features.
                 case "diff":
                     if (command.Length != 3)
                     {
@@ -63,16 +60,51 @@ namespace File_Comparison
                         break;
                     }
                     readFile(command[1], command[2]);
-                    sizeComp(); // to be continued
+                    dataCheck(); 
+                    if(same != true)
+                    {
+                        Console.WriteLine("They are different files.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("They're the same file.");
+                    }
                     break;
                 default:
-                    Console.WriteLine("Default exit");
+                    Console.WriteLine("Invalid command.");
                     break;
             }
         }
-        static void sizeComp()
+        static void dataCheck()
         {
+            if(f1.Length != f2.Length) // Checks the size of the file by the data in the file. *File* size is irrelevant in this check. 
+            {
+                same = false;
+            }
+            else
+            {
+                Console.WriteLine("Theyre the same size.");
+            }
 
+            char[] f1_ = f1.ToCharArray();
+            char[] f2_ = f2.ToCharArray();
+            int count = 0;
+            try
+            {
+                foreach (char m in f1)
+                {
+                    if (m != f2[count])
+                    {
+                        same = false;
+                    }
+                    count++;
+                }
+            }
+            catch
+            {
+                // This is just to stop it complaining if a list is bigger than another.
+            }
         }
+
     }
 }
